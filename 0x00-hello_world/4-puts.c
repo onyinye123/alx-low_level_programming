15849ac2ad4cc9c93bc89320da9696b1bd1324ca
onyinye123 <onyinyeudoba@gmail.com>
2022-09-12 16:39:07 -0700

    four puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
-/**
-* main -print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
-init main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-
-}
-^C
-
-chmod u+x 4-puts.c
+1 /* This prints a text and exits */
+2
+3 #include <stdio.h>
+4
+5 /**
+6  * main -Entry Point
+7  *
+8  * Return: Always 0  (Sucess)
+9  */
+10 int main(void)
+11 {
+12      puts("\"Programming is like building a multilingual puzzle");
+13      return (0);
+14 }
