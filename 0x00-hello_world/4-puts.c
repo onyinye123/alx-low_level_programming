f731a2e1202853cd850ead6db30d782efa2fe422
onyinye123 <onyinyeudoba@gmail.com>
2022-09-12 11:28:26 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!bin/bash
+  /* This script prints a text and exists */
+
+ #include <stdio.h>
+
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+  int main(void)
+{
+	  puts("\"Programming is like building a multilingual puzzle");
+	  return (0);
+}
