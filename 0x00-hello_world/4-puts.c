313a7b90d4d3aaab284862a2746a5685c460b9d3
onyinye123 <onyinyeudoba@gmail.com>
2022-09-12 16:58:25 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-1 /* This prints a text and exits */
-2
-3 #include <stdio.h>
-4
-5 /**
-6  * main -Entry Point
-7  *
-8  * Return: Always 0  (Sucess)
-9  */
-10 int main(void)
-11 {
-12      puts("\"Programming is like building a multilingual puzzle");
-13      return (0);
-14 }
+/* This script prints a text and exits */
+
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
