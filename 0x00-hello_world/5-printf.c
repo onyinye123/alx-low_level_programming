1ec6afb3b729c9d737c7a7adcc6df0e419a2d463
onyinye123 <onyinyeudoba@gmail.com>
2022-09-12 17:24:48 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/* This scripts prints a text and exits */
+
+#include  <stdio.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
