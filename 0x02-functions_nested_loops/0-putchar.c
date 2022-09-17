79e709943d4bdb972c42b8cbcf63ad2b35476cfe
onyinye123 <onyinyeudoba@gmail.com>
2022-09-17 02:18:37 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,41 @@
 #include "main.h"
+
 #include <string.h>
 
 
-/**
- *  main -entry point
- *
- *   print '_putchar' followed by a new line
- *
- *   Return: Always 0
- */
-int main(void)
-{
-	char str[] = "_putchar";
-	int length = strlen(str), i;
 
-	for (i =0; i < length; i++)
-		_putchar(str[i]);
-	_putchar('\n');
-	return (0);
+
+
+/**
+ *
+ *  * main - entry point
+ *
+ *   *
+ *
+ *    * print '_putchar' followed by a new line
+ *
+ *     *
+ *
+ *      * Return: Always 0
+ *
+ *       */
+
+int main(void)
+
+{
+
+		char str[] = "_putchar";
+
+			int length = strlen(str), i;
+
+
+
+				for (i = 0; i < length; i++)
+
+							_putchar(str[i]);
+
+					_putchar('\n');
+
+						return (0);
+
 }
