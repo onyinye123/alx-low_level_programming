b187fa6dd59ff35ecece6d007c1b2fbeb2ed35ec
onyinye123 <onyinyeudoba@gmail.com>
2022-09-17 02:04:09 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+void print_alphabet();
+
+void print_alphabet_×10();
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif
