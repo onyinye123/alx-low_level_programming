126ed13f54e720695e0cc84f0d05bd8499064e9c
onyinye123 <onyinyeudoba@gmail.com>
2022-09-18 04:58:01 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * + if @n is greater that zero
+ * 0 if @n is zero
+ * - if @n is less than zero
+ *    Return:
+ *    1 if @n is greater than 0
+ *    0 if @n is zero
+ *    -1 if @n is less than zero
+ *
+ *    @n: number
+ *    */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	_putchar('-');
+	return (-1);
+}
