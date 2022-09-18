acbeaf2ec5f5f797f8f24b4dd95ba9592fbdd0b3
onyinye123 <onyinyeudoba@gmail.com>
2022-09-18 11:47:14 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,181 +1,90 @@
 #include "main.h"
 
-
-
 /**
+ * increase - increase @n by 1
  *
- *  * increase - increase @n by 1
+ *  @n: a pointer to an integer
  *
- *   *
- *
- *    * @n: a pointer to an integer
- *
- *     *
- *
- *      * Return: void
- *
- *       */
-
+ *   Return: void
+ */
 void increase(int *n)
-
 {
-
-		*n = *n + 1;
-
+	*n = *n + 1;
 }
 
-
-
 /**
+ *  decrease - decrease number by 1
  *
- *  * decrease - decrease number by 1
+ *   @n: a pointer to an integer
  *
- *   *
- *
- *    * @n: a pointer to an integer
- *
- *     *
- *
- *      * Return: void
- *
- *       */
-
+ *   Return: void
+ */
 void decrease(int *n)
-
 {
-
-		*n = *n - 1;
-
+	*n = *n - 1;
 }
 
-
-
 /**
+ * gt - tests if @a is greater than or equal to @b
  *
- *  * gt - tests if @a is greater than or equal to @b
+ * @a: first number
+ * @b: second number
  *
- *   *
- *
- *    * @a: first number
- *
- *     * @b: second number
- *
- *      *
- *
- *       * Return: 1 if true, 0 otherwise
- *
- *        */
-
+ *  Return: 1 if true, 0 otherwise
+ */
 int gt(int a, int b)
-
 {
-
-		if (a >= b)
-
-					return (1);
-
-			return (0);
-
+	if (a >= b)
+		return (1);
+	return (0);
 }
 
-
-
 /**
+ *  lt - tests if @a is less than or equal to @b
  *
- *  * lt - tests if @a is less than or equal to @b
+ *   @a: first number
+ *   @b: second number
  *
- *   *
- *
- *    * @a: first number
- *
- *     * @b: second number
- *
- *      *
- *
- *       * Return: 1 if true, 0 otherwise
- *
- *        */
-
+ *   Return: 1 if true, 0 otherwise
+ */
 int lt(int a, int b)
-
 {
-
-		if (a <= b)
-
-					return (1);
-
-			return (0);
-
+	if (a <= b)
+		return (1);
+	return (0);
 }
 
-
-
-
-
 /**
+ * print_to_98 - print all natural numbers from @n to 98
  *
- *  * print_to_98 - print all natural numbers from @n to 98
+ * @n: starting point
  *
- *   *
- *
- *    * @n: starting point
- *
- *     *
- *
- *      * Return: void
- *
- *       */
-
+ *  Return: void
+ */
 void print_to_98(int n)
-
 {
+	int (*compare)(int, int);
+	void (*op)(int *);
+	int i;
 
-		int (*compare)(int, int);
+	if (n > 98)
+{
+	compare = &gt;
+	op = &decrease;
+}
+else
+{
+	compare = &lt;
+	op = &increase;
+}
 
-			void (*op)(int *);
-
-				int i;
-
-
-
-
-
-					if (n > 98)
-
-							{
-
-										compare = &gt;
-
-												op = &decrease;
-
-													}
-
-						else
-
-								{
-
-											compare = &lt;
-
-													op = &increase;
-
-														}
-
-
-
-							for (i = n; compare(i, 98); op(&i))
-
-									{
-
-												if (i == 98)
-
-																printf("%d", i);
-
-														else
-
-																		printf("%d, ", i);
-
-															}
-
-								printf("\n");
+for (i = n; compare(i, 98); op(&i))
+{
+	if (i == 98)
+		printf("%d", i);
+	else
+		printf("%d, ", i);
+}
+printf("\n");
 
 }
