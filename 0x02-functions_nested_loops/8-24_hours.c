828aa46965bea3447fa27cc65e70a6a056a479aa
onyinye123 <onyinyeudoba@gmail.com>
2022-09-18 08:57:24 -0700

    824hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
-
-
 /**
- * jack_bauer - print every minute of the day, starting from `00:00` to `23:59`
+ *  jack_bauer - print every minute of the day, starting from `00:00` to `23:59
  *
- * Return: nothing at all :)
+ *  Return: nothing at all :)
  */
 void jack_bauer(void)
 {
 
-
 	int mins, hour, min;
+
 
 	for (mins = 0; mins < (60 * 24); mins++)
 	{
@@ -19,12 +17,10 @@ void jack_bauer(void)
 		min = mins % 60;
 		_putchar(48 + (hour / 10));
 		_putchar(48 + (hour % 10));
-		_putchar(48 + (hour % 10));
 		_putchar(':');
 		_putchar(48 + (min / 10));
 		_putchar(48 + (min % 10));
 		_putchar('\n');
 	}
 }
-
 
