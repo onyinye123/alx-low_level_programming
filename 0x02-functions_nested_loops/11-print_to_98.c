3cd6f5ea6c563df4aed146f9f5ca33f828dd299a
onyinye123 <onyinyeudoba@gmail.com>
2022-09-18 11:03:54 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,61 +1,181 @@
 #include "main.h"
 
+
+
 /**
- *  increase - increase @n by 1
  *
- *   @n: a pointer to an integer
+ *  * increase - increase @n by 1
  *
- *   Return: void
- */
+ *   *
+ *
+ *    * @n: a pointer to an integer
+ *
+ *     *
+ *
+ *      * Return: void
+ *
+ *       */
+
 void increase(int *n)
+
 {
-	*n = *n + 1;
+
+		*n = *n + 1;
 
 }
 
+
+
 /**
- *  gt - tests if @a is greater than or equal to @b
  *
- *   @a: first number
- *   @b: second number
+ *  * decrease - decrease number by 1
  *
- *    Return: 1 if true, 0 otherwise
- */
+ *   *
+ *
+ *    * @n: a pointer to an integer
+ *
+ *     *
+ *
+ *      * Return: void
+ *
+ *       */
+
+void decrease(int *n)
+
+{
+
+		*n = *n - 1;
+
+}
+
+
+
+/**
+ *
+ *  * gt - tests if @a is greater than or equal to @b
+ *
+ *   *
+ *
+ *    * @a: first number
+ *
+ *     * @b: second number
+ *
+ *      *
+ *
+ *       * Return: 1 if true, 0 otherwise
+ *
+ *        */
+
 int gt(int a, int b)
+
 {
-	if (a >= b)
-		return (1);
-	return (0);
+
+		if (a >= b)
+
+					return (1);
+
+			return (0);
+
 }
+
 
 
 /**
- *  print_to_98 - print all natural numbers from @n to 98
  *
- *  @n: starting point
+ *  * lt - tests if @a is less than or equal to @b
  *
- *  Return: void
- */
+ *   *
+ *
+ *    * @a: first number
+ *
+ *     * @b: second number
+ *
+ *      *
+ *
+ *       * Return: 1 if true, 0 otherwise
+ *
+ *        */
+
+int lt(int a, int b)
+
+{
+
+		if (a <= b)
+
+					return (1);
+
+			return (0);
+
+}
+
+
+
+
+
+/**
+ *
+ *  * print_to_98 - print all natural numbers from @n to 98
+ *
+ *   *
+ *
+ *    * @n: starting point
+ *
+ *     *
+ *
+ *      * Return: void
+ *
+ *       */
+
 void print_to_98(int n)
+
 {
-	int (*compare)(int, int);
-	void (*op)(int *);
-	int i;
+
+		int (*compare)(int, int);
+
+			void (*op)(int *);
+
+				int i;
 
 
-	if (n > 98)
-	{
-		compare = &gt;
-		op = &increase;
-	}
 
-for (i = n; compare(i, 98); op(&i))
-{
-	if (i == 98)
-		printf("%d", i);
-	else
-		printf("%d, ", i);
+
+
+					if (n > 98)
+
+							{
+
+										compare = &gt;
+
+												op = &decrease;
+
+													}
+
+						else
+
+								{
+
+											compare = &lt;
+
+													op = &increase;
+
+														}
+
+
+
+							for (i = n; compare(i, 98); op(&i))
+
+									{
+
+												if (i == 98)
+
+																printf("%d", i);
+
+														else
+
+																		printf("%d, ", i);
+
+															}
+
+								printf("\n");
+
 }
-printf("\n");
-}
-
