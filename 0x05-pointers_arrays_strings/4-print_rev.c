5288cf5e6082c0f5300a83befefa223551c2f905
onyinye123 <onyinyeudoba@gmail.com>
2022-09-22 14:45:42 -0700

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,16 @@ void print_rev(char *s)
 {
 		int i; /* counter */
 
-			/* get the length of *s */
-			for (i = 0; s[i] != '\0'; i++)
-						continue; /* do nothing */
-				/* i would now be equals to the index of \0 */
-				i -= 1;
-					/* i now holds the last index */
-					while (i >= 0)
-							{
-										_putchar(s[i]);
-												i--;
-													}
-						_putchar('\n');
+		/* get the length of *s */
+		for (i = 0; s[i] != '\0'; i++)
+			continue; /* do nothing */
+		/* i would now be equals to the index of \0 */
+		i -= 1;
+		/* i now holds the last index */
+		while (i >= 0)
+		{
+			_putchar(s[i]);
+			i--;
+		}
+		_putchar('\n');
 }
