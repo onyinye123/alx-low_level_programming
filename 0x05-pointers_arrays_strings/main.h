e54f4afebca814aebc8f82c3b09f41d2a7174c7b
onyinye123 <onyinyeudoba@gmail.com>
2022-09-22 12:14:15 -0700

    strcat

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
-#define MAIN_H 
+#define MAIN_H
 
 /**
- *  void prototypes()
- *  int  prototypes() 
+ * void prototypes()
+ * int  prototypes()
  */
 
 void reset_to_98(int *n);
@@ -11,10 +11,10 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
- void rev_string(char *s);
- void puts2(char *str);
- void puts_half(char *str);
- void print_array(int *a, int n);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
 
