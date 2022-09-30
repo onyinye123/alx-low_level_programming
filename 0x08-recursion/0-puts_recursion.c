b371a58b3c573250f97bb8949fa16e6c30d7fbcf
onyinye123 <onyinyeudoba@gmail.com>
2022-09-29 17:31:50 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{ 
-		 _putchar(*s);
-		 _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		 _putchar('\n');
 	}
 	else
-		_putchar('\n');
-}
+	{
+		_putchar(s[0]);
+		 _puts_recursion(s + 1);
+	}
+
+} 
