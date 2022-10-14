b3c848fa2ee1b4a31ea335a918a034ee7d5a50ce
onyinye123 <onyinyeudoba@gmail.com>
2022-10-13 21:22:16 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,6 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
+	if (n==0)
+		return (0);
+
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
